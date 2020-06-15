6e35f80fac7e99240f67fe46472b6a1d4985f634
Harshit Gajjar <hrstgajjar3@gmail.com>
2020-06-15 17:29:00 +0400

    MeVC added to project

---
+++ ChatApp/Controller/MeVC.swift
@@ -0,0 +1,24 @@
+//
+//  MeVC.swift
+//  ChatApp
+//
+//  Created by Harshit Gajjar on 6/15/20.
+//  Copyright © 2020 ThinkX. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var username: UILabel!
+    @IBOutlet weak var userdetails: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func logout(_ sender: Any) {
+    }
+}
